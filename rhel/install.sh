0783650f939beff81aafbf26caee38e8afb6718a
Harald Hauknes <harald.hauknes@combitech.no>
2024-04-12 14:03:37 +0200

    rewrite to rhel

--- rhel/install.sh
+++ rhel/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#
-# This script is for Ubuntu 22.04 Jammy Jellyfish to download and install XRDP+XORGXRDP via
-# source.
-#
-# Major thanks to: http://c-nergy.be/blog/?p=11336 for the tips.
+# This script is for RHEL9-based distros (AlmaLinux, RockyLinux, OracleLinux) to download and install XRDP+XORGXRDP.
+# It aims to enable Enhanced Session mode for Hyper-V.
+# Major thanks to: https://www.nakivo.com/blog/install-ubuntu-20-04-on-hyper-v-with-enhanced-session
+# and to Hinara/linux-vm-tools ong Github for maintaining the now archived Microsoft-[repo](https://github.com/microsoft/linux-vm-tools)
 #
 
 ###############################################################################
+# NOTE: This version of the script does not use this, its left for debugging
 # Use HWE kernel packages
 #
-HWE=""
+#HWE=""
 #HWE="-hwe-22.04"
 
 ###############################################################################
@@ -22,24 +22,28 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-apt update && apt upgrade -y
+#update && apt upgrade -y
+dnf update -y && dnf install -y epel-release
 
-if [ -f /var/run/reboot-required ]; then
-    echo "A reboot is required in order to proceed with the install." >&2
-    echo "Please reboot and re-run this script to finish the install." >&2
-    exit 1
-fi
+# Note a reboot is not needed on this version of the script
+#if [ -f /var/run/reboot-required ]; then
+#    echo "A reboot is required in order to proceed with the install." >&2
+#    echo "Please reboot and re-run this script to finish the install." >&2
+#    exit 1
+#fi
 
 ###############################################################################
 # XRDP
 #
 
+# Note: NO
 # Install hv_kvp utils
-apt install -y linux-tools-virtual${HWE}
-apt install -y linux-cloud-tools-virtual${HWE}
+# apt install -y linux-tools-virtual${HWE}
+# apt install -y linux-cloud-tools-virtual${HWE}
 
 # Install the xrdp service so we have the auto start behavior
-apt install -y xrdp
+#apt install -y xrdp
+dnf install -y xrdp
 
 systemctl stop xrdp
 systemctl stop xrdp-sesman
@@ -54,8 +58,10 @@ sed -i_orig -e 's/crypt_level=high/crypt_level=none/g' /etc/xrdp/xrdp.ini
 # disable bitmap compression since its local its much faster
 sed -i_orig -e 's/bitmap_compression=true/bitmap_compression=false/g' /etc/xrdp/xrdp.ini
 
+# FIXME: Lets skip this for now
 # Add script to setup the ubuntu session properly
-if [ ! -e /etc/xrdp/startubuntu.sh ]; then
+#if [ ! -e /etc/xrdp/startubuntu.sh ]; then
+if [ ! -e /etc ]; then
 cat >> /etc/xrdp/startubuntu.sh << EOF
 #!/bin/sh
 export GNOME_SHELL_SESSION_MODE=ubuntu
@@ -65,13 +71,17 @@ EOF
 chmod a+x /etc/xrdp/startubuntu.sh
 fi
 
+#FIXME: NO
 # use the script to setup the ubuntu session
-sed -i_orig -e 's/startwm/startubuntu/g' /etc/xrdp/sesman.ini
+#sed -i_orig -e 's/startwm/startubuntu/g' /etc/xrdp/sesman.ini
 
 # rename the redirected drives to 'shared-drives'
 sed -i -e 's/FuseMountName=thinclient_drives/FuseMountName=shared-drives/g' /etc/xrdp/sesman.ini
 
 # Changed the allowed_users
+if [ ! -e /etc/X11/Xwrapper.config ]; then
+echo allowed_users=anybody >> /etc/X11/Xwrapper.config
+fi
 sed -i_orig -e 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
 
 # Blacklist the vmw module
@@ -103,4 +113,4 @@ systemctl start xrdp
 ###############################################################################
 
 echo "Install is complete."
-echo "Reboot your machine to begin using XRDP."
+#echo "Reboot your machine to begin using XRDP."
