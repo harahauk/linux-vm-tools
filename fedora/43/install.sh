20f048fbbab0be34050dd3625e885700d8606e91
Harald Hauknes <harald@hauknes.org>
2026-02-17 20:22:09 +0100

    duplicate of rhel-folder

---
+++ fedora/43/install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+##
+#  Configures a RHEL 9-based (AlmaLinux, RockuLinux, etc.) VM to support 
+#  Hyper-V Enhanced Session Mode using XRDP and Xorg.
+##
+
+
+# Ensure that script is running as root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script must be run as root. Please run with sudo or as root user."
+  exit 1
+fi
+
+# Package repositories
+echo "Adding necessary package repositories (EPEL, CRB) and updating system..."
+dnf install -y epel-release
+/usr/bin/crb enable # Not sure if actually needed # TODO: review without
+dnf update -y
+
+# Installing Xorg/X11 and XRDP
+echo "Installing Xorg/X11 and XRDP packages.."
+dnf install -y \
+  xrdp \
+  xorgxrdp \
+  xorg-x11-server-Xorg
+
+echo "Configuring XRDP.."
+#TODO: Use pwd and omit path if needed
+chmod +x ./linux-vm-tools/common/configure_xrdp.sh
+./linux-vm-tools/common/configure_xrdp.sh
+# Enable and start XRDP service
+echo "Enabling and starting XRDP service.."
+#TODO: s/config
+systemctl enable --now xrdp xrdp-sesman
+echo "Backend configured. If you have a working window manager, you should be able to connect after the next step."
+echo "Action required once: Run 'Set-VM -VMName <the_name_of_this_VM> -EnhancedSessionTransportType HvSocket' in an elevated Powershell promt while this VM is turned off"
+echo "Then start the VM and connect using Enhanced Session Mode."
+echo "Need a window manager? Run './linux-vm-tools/common/install_i3.sh' with sudo to install i3wm. Gnome/KDE coming in future releases."
