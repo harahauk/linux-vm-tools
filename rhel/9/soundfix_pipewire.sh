c6b18fec92ac34d8d2e836b16bec249815987c94
Harald Hauknes <harald@hauknes.org>
2026-03-07 20:46:25 +0100

    initial commit

---
+++ rhel/9/soundfix_pipewire.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+##
+#  Enables sound over xRDP (Hyper-V enhanced-session) for pipewire-based systems
+#  Builds and installs https://github.com/neutrinolabs/pipewire-module-xrdp
+#  @author Harald Hauknes <harald@hauknes.org>
+##
+
+echo "Installing pipewire-utils for Xorg and CLI"
+sudo dnf install -y pipewire-module-x11 pipewire-pulseaudio pipewire-utils
+echo "Installing dependencies for building 'pipewire-module-xrdp'"
+sudo dnf install -y git gcc make autoconf libtool automake pkgconfig
+echo "Installing libraries sourced under package building"
+sudo dnf install -y pipewire-devel
+echo "Getting the module from Github"
+git clone https://github.com/neutrinolabs/pipewire-module-xrdp /tmp/pipewire-module-xrdp
+cd /tmp/pipewire-module-xrdp
+# TODO: Get specific working tag or branch, now default to 'devel'
+echo "Attempting to build module"
+./bootstrap
+./configure
+make
+echo "Installing module"
+sudo make install
+
